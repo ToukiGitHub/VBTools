9d22ec4115be9c5448682aaf9275def1c6524afc
T.K <152837269+ToukiGitHub@users.noreply.github.com>
2023-12-19 17:41:21 +0900

    パスワード生成ツール完成

--- CreatPassword.Designer.vb
+++ CreatPassword.Designer.vb
@@ -30,6 +30,11 @@ Partial Class CreatPassword
         CheckBox4 = New CheckBox()
         FlowLayoutPanel1 = New FlowLayoutPanel()
         CheckBox5 = New CheckBox()
+        Label3 = New Label()
+        ComboBox1 = New ComboBox()
+        Button1 = New Button()
+        Label4 = New Label()
+        Button2 = New Button()
         SuspendLayout()
         ' 
         ' Label1
@@ -48,7 +53,7 @@ Partial Class CreatPassword
         Label2.AutoSize = True
         Label2.Font = New Font("Yu Gothic UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
         Label2.ForeColor = Color.SpringGreen
-        Label2.Location = New Point(12, 137)
+        Label2.Location = New Point(30, 137)
         Label2.Name = "Label2"
         Label2.Size = New Size(152, 32)
         Label2.TabIndex = 1
@@ -59,7 +64,7 @@ Partial Class CreatPassword
         CheckBox1.AutoSize = True
         CheckBox1.Font = New Font("Yu Gothic UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
         CheckBox1.ForeColor = Color.SpringGreen
-        CheckBox1.Location = New Point(165, 135)
+        CheckBox1.Location = New Point(177, 135)
         CheckBox1.Name = "CheckBox1"
         CheckBox1.Size = New Size(153, 36)
         CheckBox1.TabIndex = 2
@@ -71,7 +76,7 @@ Partial Class CreatPassword
         CheckBox2.AutoSize = True
         CheckBox2.Font = New Font("Yu Gothic UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
         CheckBox2.ForeColor = Color.SpringGreen
-        CheckBox2.Location = New Point(367, 135)
+        CheckBox2.Location = New Point(359, 135)
         CheckBox2.Name = "CheckBox2"
         CheckBox2.Size = New Size(153, 36)
         CheckBox2.TabIndex = 3
@@ -83,7 +88,7 @@ Partial Class CreatPassword
         CheckBox3.AutoSize = True
         CheckBox3.Font = New Font("Yu Gothic UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
         CheckBox3.ForeColor = Color.SpringGreen
-        CheckBox3.Location = New Point(569, 135)
+        CheckBox3.Location = New Point(541, 135)
         CheckBox3.Name = "CheckBox3"
         CheckBox3.Size = New Size(81, 36)
         CheckBox3.TabIndex = 4
@@ -104,9 +109,9 @@ Partial Class CreatPassword
         ' 
         ' FlowLayoutPanel1
         ' 
-        FlowLayoutPanel1.Location = New Point(165, 217)
+        FlowLayoutPanel1.Location = New Point(175, 217)
         FlowLayoutPanel1.Name = "FlowLayoutPanel1"
-        FlowLayoutPanel1.Size = New Size(563, 100)
+        FlowLayoutPanel1.Size = New Size(563, 130)
         FlowLayoutPanel1.TabIndex = 7
         ' 
         ' CheckBox5
@@ -114,19 +119,78 @@ Partial Class CreatPassword
         CheckBox5.AutoSize = True
         CheckBox5.Font = New Font("Yu Gothic UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
         CheckBox5.ForeColor = Color.SpringGreen
-        CheckBox5.Location = New Point(165, 177)
+        CheckBox5.Location = New Point(177, 177)
         CheckBox5.Name = "CheckBox5"
         CheckBox5.Size = New Size(159, 36)
         CheckBox5.TabIndex = 8
         CheckBox5.Text = "すべての記号"
         CheckBox5.UseVisualStyleBackColor = True
         ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Yu Gothic UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Label3.ForeColor = Color.SpringGreen
+        Label3.Location = New Point(6, 356)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(176, 32)
+        Label3.TabIndex = 9
+        Label3.Text = "パスワード文字数"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.DropDownHeight = 100
+        ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox1.Font = New Font("Yu Gothic UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.IntegralHeight = False
+        ComboBox1.Location = New Point(189, 352)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(74, 40)
+        ComboBox1.TabIndex = 10
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Yu Gothic UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Button1.Location = New Point(270, 352)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(224, 40)
+        Button1.TabIndex = 11
+        Button1.Text = "パスワード生成"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Yu Gothic UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Label4.ForeColor = Color.SpringGreen
+        Label4.Location = New Point(322, 427)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(161, 50)
+        Label4.TabIndex = 12
+        Label4.Text = "パスワード"
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Yu Gothic UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Button2.Location = New Point(501, 352)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(236, 40)
+        Button2.TabIndex = 13
+        Button2.Text = "パスワードをコピー"
+        Button2.UseVisualStyleBackColor = True
+        ' 
         ' CreatPassword
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.CornflowerBlue
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(804, 511)
+        Controls.Add(Button2)
+        Controls.Add(Label4)
+        Controls.Add(Button1)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label3)
         Controls.Add(CheckBox5)
         Controls.Add(FlowLayoutPanel1)
         Controls.Add(CheckBox4)
@@ -149,4 +213,9 @@ Partial Class CreatPassword
     Friend WithEvents CheckBox4 As CheckBox
     Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
     Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
 End Class
--- CreatPassword.vb
+++ CreatPassword.vb
@@ -1,13 +1,11 @@
 ﻿
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar
+
 Public Class CreatPassword
-    Public arrUpperLetter = New String() {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-    Public arrLowerLetter = New String() {"a", "b", "c", "d", "e", "f", "g", "g", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-    Public arrNum = New String() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
-    Public arrSymbol = New String() {"/", "*", "-", "+", ".", "!", "#", "$", "%", "(", ")", "~", "|", "_", "@", "[", "]", "^"}
     Private Sub FormMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         myModule.LoadForm(Me)
         AddSymbolCheckbox()
-
+        SetCombobox()
     End Sub
 
     Private Sub CreatPassword_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
@@ -20,16 +18,17 @@ Public Class CreatPassword
         CheckBox3.Checked = True
         CheckBox4.Checked = True
         CheckBox5.Checked = True
-        For Each it In arrSymbol
+        Dim arySymbol = New String() {"/", "*", "-", "+", ".", "!", "#", "$", "%", "(", ")", "~", "|", "_", "@", "[", "]", "^"}
+        For Each it In arySymbol
             Dim checkBox As New CheckBox()
             checkBox.Text = it
-            checkBox.Size = New Size(60, 50)
+            checkBox.Size = New Size(55, 50)
             checkBox.Font = New Font("Yu Gothic UI", 18, FontStyle.Bold)
             checkBox.ForeColor = Color.White
             ' PanelにCheckBoxを追加
             FlowLayoutPanel1.Controls.Add(checkBox)
-            checkBox.Checked = True
         Next
+        SetSymbolValue(True)
     End Sub
 
     Private Sub SetSymbolValue(ByVal v As Boolean)
@@ -37,14 +36,34 @@ Public Class CreatPassword
             If TypeOf control Is CheckBox Then
                 Dim checkBox As CheckBox = DirectCast(control, CheckBox)
                 checkBox.Checked = v
-                If CheckBox4.Checked = True Then
-                    checkBox.Enabled = True
-                Else
+                If CheckBox4.Checked = False Or CheckBox5.Checked = True Then
                     checkBox.Enabled = False
+                Else
+                    checkBox.Enabled = True
                 End If
             End If
         Next
     End Sub
+    Private Sub SetCombobox()
+        Dim intPasswordLen As Integer
+        For intPasswordLen = 8 To 32
+            ComboBox1.Items.Add(intPasswordLen)
+        Next
+        ComboBox1.SelectedIndex = 4
+    End Sub
+
+    Private Function GetSelectedSymbol() As String
+        Dim strSymbol As String = ""
+        For Each control As Control In FlowLayoutPanel1.Controls
+            If TypeOf control Is CheckBox Then
+                Dim checkBox As CheckBox = DirectCast(control, CheckBox)
+                If checkBox.Checked Then
+                    strSymbol += checkBox.Text
+                End If
+            End If
+        Next
+        Return strSymbol
+    End Function
 
     Private Sub CheckBox4_Click(sender As Object, e As EventArgs) Handles CheckBox4.Click
         If CheckBox4.Checked = True Then
@@ -58,5 +77,55 @@ Public Class CreatPassword
     End Sub
     Private Sub CheckBox5_Click(sender As Object, e As EventArgs) Handles CheckBox5.Click
         SetSymbolValue(CheckBox5.Checked)
+    End Sub
+
+    Private Sub CreatePassword()
+        Dim strUpperLetter As String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        Dim strLowerLetter As String = "abcdefggijklmnopqrstuvwxyz"
+        Dim strNum As String = "0123456789"
+        Dim strSource As String = ""
+        Dim strPassword As String = ""
+        '選択された文字種を最低1文字を使われるように
+        If CheckBox1.Checked Then
+            strPassword += strUpperLetter.Substring(Random.Shared.Next(26), 1)
+            strSource += strUpperLetter
+        End If
+        If CheckBox2.Checked Then
+            strPassword += strLowerLetter.Substring(Random.Shared.Next(26), 1)
+            strSource += strLowerLetter
+        End If
+        If CheckBox3.Checked Then
+            strPassword += strNum.Substring(Random.Shared.Next(10), 1)
+            strSource += strNum
+        End If
+        Dim strSymbol As String = GetSelectedSymbol()
+        Dim intSymbolLen As Integer = strSymbol.Length
+        If intSymbolLen > 0 Then
+            strPassword += strSymbol.Substring(Random.Shared.Next(intSymbolLen), 1)
+            strSource += strSymbol
+        End If
+        Dim intSourceLen As Integer = strSource.Length
+        Dim intPasswordLen As Integer = strPassword.Length
+        '文字種1個以上選択されているかどうかを確認
+        If intPasswordLen = 0 Then
+            Label4.Text = "パスワード構成が選択されていません！"
+            Label4.ForeColor = Color.DarkRed
+        Else
+            intPasswordLen = ComboBox1.SelectedItem - intPasswordLen
+            For i = 1 To intPasswordLen
+                strPassword += strSource.Substring(Random.Shared.Next(intSourceLen), 1)
+            Next
+            Label4.Text = strPassword
+            Label4.ForeColor = Color.SpringGreen
+        End If
+        Label4.Location = New Point((Me.Width - Label4.Width) \ 2, Label4.Top)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CreatePassword()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Clipboard.SetText(Label4.Text)
     End Sub
 End Class
